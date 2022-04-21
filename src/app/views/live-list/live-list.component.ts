1049b9a39ece5329cff508dfc80765ec6895c1fd
Caitano <josiasmartinscaitano@gmail.com>
2022-04-21 09:05:52 -0300

    fixture: obtenado dados da API externa

--- src/app/views/live-list/live-list.component.ts
+++ src/app/views/live-list/live-list.component.ts
@@ -1,3 +1,5 @@
+import { Live } from './../../shared/model/live.model';
+import { LiveService } from './../../shared/service/live.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveListComponent implements OnInit {
 
-  constructor() { }
+  livesPrevious: Live[];
+  livesNext: Live[];
+
+  constructor(
+    public liveServer: LiveService
+  ) { }
 
   ngOnInit(): void {
+    this.getLives();
   }
 
+  getLives() {
+    this.liveServer.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious)
+    });
+
+    this.liveServer.getLivesWithFlag('next').subscribe(data => {
+      this.livesNext = data.content;
+      console.log(this.livesPrevious);
+    })
+  }
 }
